1a058e614058b7d6f60bbfdc17d1968cb9f0f22c
sayalikarnewar <sayali.karnewar@gmail.com>
2020-08-30 13:24:27 +0530

    commit on master

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("testing");
+console.log(1);
+
+let name = "Sayalikarnewar";
+console.log(name[6]);
+
+console.log(name.toUpperCase());
+
+let result = name.toLowerCase();
+console.log(result,name );
+
+let k = name.indexOf('a');
+console.log(k);
+
+let l = name.lastIndexOf('a');
+console.log(l);
+
+console.log(name.slice(0,3));
+
+console.log(name.substr(0,3));
+
+console.log(name.replace('a', '@'));
+
+names = ['saya', 'swar', 'praju'];
+console.log(names.length);
+
+console.log(names.join('-'));
+
+sur = ['kar', 'kj'];
+console.log(names.concat(sur));
+
+console.log(names.push('praf'));
+names.pop();
+console.log(names);
