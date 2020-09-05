59c2cfc8bf8c0e3181511183630e4ef927b2653e
sayalikarnewar <sayali.karnewar@gmail.com>
2020-09-05 23:08:15 +0530

    commit to master

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quoteContainer = document.getElementById('quote-container');
 const quotetext = document.getElementById('quote');
 const author = document.getElementById('author');
-const share = document.getElementById('share');
+const twitter = document.getElementById('share-button');
 const next = document.getElementById('new-quote');
 
 //get quote from API
@@ -13,13 +13,43 @@ async function getQuote(){
     try{
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        author.innerHTML = data.quoteAuthor;
-        quotetext.innerHTML = data.quoteText;
 
+        if(data.quoteAuthor == ""){
+            author.innerText == 'Unknown';
+        }
+        else{
+            author.innerText = data.quoteAuthor;
+        }   
+
+        if (data.quoteText.length > 50){
+            quotetext.classList.add('long-quote');
+        }
+        else{
+            quotetext.classList.remove('long-quote');
+        }
+
+        quotetext.innerText = data.quoteText;
+        
     }catch(error){
 
         getQuote();
     }
 };
+
+
+//tweet quote
+
+function share(){
+    const tweet = quotetext.innerText;
+    const authorB = author.innerText;
+    const url = `https://twitter.com/intent/tweet?text=${tweet} - ${authorB}`;
+    window.open(url, '_blank');
+}
+
+
+//event listener
+next.addEventListener('click', getQuote);
+twitter.addEventListener('click', share);
+
 
 getQuote();
