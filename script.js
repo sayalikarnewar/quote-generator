1b761b5f12dddc182a20d572f17a781ca24d55e3
sayalikarnewar <sayali.karnewar@gmail.com>
2020-09-05 18:09:57 +0530

    commit to master

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //get quote from API
 
+async function getQuote(){
+    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json(apiUrl);
+        console.log(data);
+        
+    }catch(error){
+        console.log("oops",error);
+    }
+};
+
+getQuote();
