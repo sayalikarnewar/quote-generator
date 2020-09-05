095599953c46bd45b016d3f4c2d0f54e9964c1c3
sayalikarnewar <sayali.karnewar@gmail.com>
2020-09-05 18:32:53 +0530

    commit to master

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+const quoteContainer = document.getElementById('.quote-container');
+const quotetext = document.getElementById('.quote-text');
+const author = document.getElementById('.author');
+const share = document.getElementById('.share');
+const next = document.getElementById('.new-quote');
+
 //get quote from API
 
 async function getQuote(){
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
     const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try{
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json(apiUrl);
-        console.log(data);
-        
+        author.innerText = data.author;
+        quotetext.innerText = data.quotetext;
+
     }catch(error){
-        console.log("oops",error);
+        getQuote();
+        
     }
 };
 
