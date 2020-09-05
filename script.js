9fc85a8d2a5e71de44ccd4f10094af9fc3be59a6
sayalikarnewar <sayali.karnewar@gmail.com>
2020-09-05 18:54:58 +0530

    commit to master

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const quoteContainer = document.getElementById('.quote-container');
-const quotetext = document.getElementById('.quote-text');
-const author = document.getElementById('.author');
-const share = document.getElementById('.share');
-const next = document.getElementById('.new-quote');
+const quoteContainer = document.getElementById('quote-container');
+const quotetext = document.getElementById('quote');
+const author = document.getElementById('author');
+const share = document.getElementById('share');
+const next = document.getElementById('new-quote');
 
 //get quote from API
 
 async function getQuote(){
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
-    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    
     try{
         const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json(apiUrl);
-        author.innerText = data.author;
-        quotetext.innerText = data.quotetext;
+        const data = await response.json();
+        author.innerHTML = data.quoteAuthor;
+        quotetext.innerHTML = data.quoteText;
 
     }catch(error){
+
         getQuote();
-        
     }
 };
 
